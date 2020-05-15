0cb028440bd036c5b7328ecbf89beb108a5d4529
stuart <stuart@bearclawsolutions.co.uk>
2020-05-15 16:57:25 +0100

    Initial creation of the user profile page

--- securityTemplate/src/app/profile/profile.page.ts
+++ securityTemplate/src/app/profile/profile.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../auth/auth.service";
-import {HomeService} from "../home/home.service";
 import {ProfileService} from "./profile.service";
 import {UserProfile} from "../models/UserProfile";
+import {FormGroup} from "@angular/forms";
+import {AlertController} from "@ionic/angular";
 
 @Component({
   selector: 'app-profile',
@@ -10,27 +11,62 @@ import {UserProfile} from "../models/UserProfile";
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  userName: string;
-  userProfile: UserProfile;
+    userName: string;
+    userProfile: UserProfile;
+    isLoading = false;
+    form: FormGroup;
 
-  constructor(private authService: AuthService,
-              private profileService: ProfileService) { }
+    constructor(private authService: AuthService,
+                private profileService: ProfileService,
+                private alertController: AlertController) { }
 
-  ngOnInit() {
+    ngOnInit() {
     this.authService.getCurrentUser()
         .subscribe(user => {
           console.log(user);
           this.userName = user.username;
           this.getProfile();
         });
-  }
+    }
 
-  getProfile() {
+    getProfile() {
+    this.isLoading = true;
     this.profileService.getUserProfile(this.userName)
         .subscribe(profile => {
-          console.log(profile);
-          this.userProfile = profile;
+            console.log(profile);
+            this.userProfile = profile;
+            this.isLoading = false;
         })
-  }
+    }
+
+    editName() {
+        const alert = this.alertController.create({
+            header: 'Enter name',
+            inputs: [
+                {
+                    name: 'name',
+                    placeholder: 'Name',
+                    value: this.userProfile.name
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Ok',
+                    handler: data => {
+                        this.userProfile.name = data.name;
+                    }
+                }
+            ]
+        }).then(alertEl => {
+            alertEl.present();
+        });
+    }
 
 }
